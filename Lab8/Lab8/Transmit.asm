590668b3fad50c65fb80d17cbc1bc60583de3339
Austin Wilmoth <wilmotha@oregonstate.edu>
2019-03-10 16:37:11 -0700

    Comments added to Transmit file

--- Lab8/Lab8/Transmit.asm
+++ Lab8/Lab8/Transmit.asm
@@ -2,14 +2,14 @@
 ;*
 ;*  Transmit
 ;*
-;*	Enter the description of the program here
+;*	This Program is used to send commands from one AVR board
 ;*
-;*	This is the TRANSMIT skeleton file for Lab 8 of ECE 375
+;*	to another that is programed to controll a robot
 ;*
 ;***********************************************************
 ;*
 ;*	 Author: Austin Wilmoth and Donald Joyce
-;*	   Date: Enter Date
+;*	   Date: March 11, 2019
 ;*
 ;***********************************************************
 
@@ -19,13 +19,15 @@
 ;*	Internal Register Definitions and Constants
 ;***********************************************************
 .def	mpr = r16				; Multi-Purpose Register
-.def	address = r17
-.def	transfer = r18
+.def	address = r17			; holds the bot address
+.def	transfer = r18			; holds the command to be transfered
 
 .equ	EngEnR = 4				; Right Engine Enable Bit
 .equ	EngEnL = 7				; Left Engine Enable Bit
 .equ	EngDirR = 5				; Right Engine Direction Bit
 .equ	EngDirL = 6				; Left Engine Direction Bit
+
+
 ; Use these action codes between the remote and robot
 ; MSB = 1 thus:
 ; control signals are shifted right by one and ORed with 0b10000000 = $80
@@ -34,7 +36,7 @@
 .equ	TurnR =   ($80|1<<(EngDirL-1))					;0b10100000 Turn Right Action Code
 .equ	TurnL =   ($80|1<<(EngDirR-1))					;0b10010000 Turn Left Action Code
 .equ	Halt =    ($80|1<<(EngEnR-1)|1<<(EngEnL-1))		;0b11001000 Halt Action Code
-.equ	Freeze =  0b11111000
+.equ	Freeze =  0b11111000		                    ;0b11111000 Freeze Action Code
 ;***********************************************************
 ;*	Start of Code Segment
 ;***********************************************************
@@ -45,18 +47,6 @@
 ;***********************************************************
 .org	$0000					; Beginning of IVs
 		rjmp 	INIT			; Reset interrupt
-;.org	$0002
-		;rjmp	MoveForward
-;.org	$0004
-;		rjmp	MoveBackward
-;.org	$000A
-;		rjmp	TurnRight
-;.org	$000C
-;		rjmp	TurnLeft
-;.org	$000E
-;		rjmp	Halt_Sub
-;.org	$0010
-;		rjmp	Freeze
 
 .org	$0046					; End of Interrupt Vectors
 
@@ -70,170 +60,171 @@ INIT:
 	ldi mpr, low(RAMEND)
 	out spl, mpr
 
-	ldi mpr, $FF
-	out DDRB, mpr
+	;Ports
+	ldi mpr, $FF	; B is set up for output to be able to control 
+	out DDRB, mpr	; the lights, for testing
 
-	ldi mpr, $00
-	out DDRD, mpr
+	ldi mpr, $00	; D is set up for input to both take in button
+	out DDRD, mpr	; presses and to recive the TXD1 interupt
 
-	ldi mpr, $FF
-	out PortD, mpr
+	ldi mpr, $FF	; PortD is set to all ones as the buttons are
+	out PortD, mpr	; passive
 
-	ldi address, $4D;0b01010101
+	ldi address, $4D	; this is the bot address
 
 
 	;USART1
 	;Set baudrate at 2400bps
-	
 	ldi mpr, high(832)
 	sts UBRR1H, mpr
 	ldi mpr, low(832)
 	sts UBRR1L, mpr
 
-	;Enable transmitter
-	ldi mpr, (1<<U2X1)
+	;Enable transmitter 
+	ldi mpr, (1<<U2X1)	; sets to double boadrate
 	sts UCSR1A, mpr
 
-	ldi mpr, (1<<TXEN1)
+	ldi mpr, (1<<TXEN1) ; set to enable transmition
 	sts UCSR1B, mpr
 
-	
-	;Set frame format: 8 data bits, 2 stop bits
-	
+	;Set frame format: 8 data bits, 2 stop 
 	ldi mpr, (1<<USBS1)|(1<<UCSZ11)|(1<<UCSZ10)
 	sts UCSR1C, mpr
 
-	;Buttons Interupts
-	;clr mpr
-	;ldi mpr, 0b00101010
-	;sts EICRA, mpr
-	;ldi mpr, 0b00101010
-	;out EICRB, mpr
-
-	;ldi mpr, 0b00110111
-	;out EIMSK, mpr
-
-	sei
-	;Other
-
+	sei					; enable interupts
+	
 ;***********************************************************
 ;*	Main Program
 ;***********************************************************
 MAIN
-	in mpr, PIND
-	cpi mpr, 0b11111110
+	in mpr, PIND			; read in from the buttons
+	cpi mpr, 0b11111110		; check if the button 0 is pressed
 	brne Bck
-	rcall MoveForward
+	rcall MoveForward		
 	rjmp	MAIN
 Bck:
-	cpi mpr, 0b11111101
+	cpi mpr, 0b11111101		; check if button 1 is pressed
 	brne TrnR
 	rcall MoveBackward
 	rjmp	MAIN
 
 TrnR:
-	cpi mpr, 0b11101111
-	brne TrnL
+	cpi mpr, 0b11101111		; check if button 4 is pressed
+	brne TrnL				; skip 2 and 3 as the are used by USART
 	rcall TurnRight
 	rjmp	MAIN
 TrnL:
-	cpi mpr, 0b11011111
+	cpi mpr, 0b11011111		; check if button 5 is pressed
 	brne Hlt
 	rcall TurnLeft
 	rjmp	MAIN
 Hlt:
-	cpi mpr, 0b10111111
+	cpi mpr, 0b10111111		; check if button 6 is pressed
 	brne Fre
 	rcall Halt_Sub
 	rjmp	MAIN
 Fre:
-	cpi mpr, 0b01111111
+	cpi mpr, 0b01111111		; check if button 7 is pressed
 	brne Skip2
 	rcall Freeze_sub
-	rjmp Main
+	rjmp Main				
 Skip2:
 	
-	rjmp	MAIN
+	rjmp	MAIN			; otherwise start over
 
 ;***********************************************************
 ;*	Functions and Subroutines
 ;***********************************************************
+;----------------------------------------------------------------
+; Sub: MoveForward
+; Desc: Sets the code to be transfered to be the code to move the
+;       the bot forward	
+;----------------------------------------------------------------
 MoveForward:
-	
-	ldi transfer, MovFwd
-	
+	ldi transfer, MovFwd		
 	rcall Transmit
-
-	ldi mpr, $FF
-	out EIFR, mpr
+	
 	ret
 
+;----------------------------------------------------------------
+; Sub: MoveBackwards
+; Desc: Sets the code to be transfered to be the code to move the
+;       the bot backwards	
+;----------------------------------------------------------------
 MoveBackward:
-
 	ldi transfer, MovBck
-	
 	rcall Transmit
 
-	ldi mpr, $FF
-	out EIFR, mpr
 	ret
 
+;----------------------------------------------------------------
+; Sub: TurnRight
+; Desc: Sets the code to be transfered to be the code to make the
+;       bot turn right 	
+;----------------------------------------------------------------
 TurnRight:
-
 	ldi transfer, TurnR
-	
 	rcall Transmit
 
-	ldi mpr, $FF
-	out EIFR, mpr
 	ret
 
+;----------------------------------------------------------------
+; Sub: TurnLeft
+; Desc: Sets the code to be transfered to be the code to make the
+;       bot turn Left 	
+;----------------------------------------------------------------
 TurnLeft:
 	ldi transfer, TurnL
-	
 	rcall Transmit
 
-	ldi mpr, $FF
-	out EIFR, mpr
 	ret
-
+;----------------------------------------------------------------
+; Sub: Halt_Sub
+; Desc: Sets the code to be transfered to be the code to make the
+;       bot stop moving	
+;----------------------------------------------------------------
 Halt_Sub:
 	ldi transfer, Halt
-	
 	rcall Transmit
-
-	ldi mpr, $FF
-	out EIFR, mpr
+	
 	ret
 
+;----------------------------------------------------------------
+; Sub: Freeze_Sub
+; Desc: Sets the code to be transfered to be the code to make the
+;       bot send out the freeze command to other bots 	
+;----------------------------------------------------------------
 Freeze_sub:
 	ldi transfer, Freeze
-	
 	rcall Transmit
-
 
 	ret
 
-
+;----------------------------------------------------------------
+; Sub: Transmit
+; Desc: Sends both the address of the bot and the command that is
+;       to be done by the bot
+;----------------------------------------------------------------
 Transmit:
-	LDS mpr, UCSR1A
+	LDS mpr, UCSR1A		; check that the transmit buffer is empty
 	SBRS mpr, UDRE1
-	rjmp Transmit
-	STS UDR1, address
+	rjmp Transmit		; if not loop 
+	
+	STS UDR1, address	; if so put the address into UDR1 to be sent
 
 	Loop_1:
-		LDS mpr, UCSR1A
-		SBRS mpr, UDRE1
+		LDS mpr, UCSR1A		; check again that the transmit buffer if
+		SBRS mpr, UDRE1		; empty meaning that the first thing is sent
 		rjmp Loop_1
-		STS UDR1, transfer
-		;out portb, transfer; test
+		
+		STS UDR1, transfer	; if it is send that command
 
 	Loop_2:
-		LDS mpr, UCSR1A
+		LDS mpr, UCSR1A		; check if the data has finished transmiting
 		SBRS mpr, TXC1
 		rjmp Loop_2
 		
-		cbr mpr, TXC1
+		cbr mpr, TXC1		; if so clear the finished bit 
 		STS UCSR1A, mpr
 
 
